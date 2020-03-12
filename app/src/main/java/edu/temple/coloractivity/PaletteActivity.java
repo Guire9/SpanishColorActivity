647456696bb3de2fe2217b6e83e4b08d1ae34a6a
Maguire <maguireqvale@gmail.com>
2020-03-12 02:04:16 -0400

    All works except funky colors

--- app/src/main/java/edu/temple/coloractivity/PaletteActivity.java
+++ app/src/main/java/edu/temple/coloractivity/PaletteActivity.java
@@ -12,6 +12,7 @@ import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class PaletteActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     RelativeLayout relativeLayout;
@@ -33,15 +34,50 @@ public class PaletteActivity extends AppCompatActivity implements AdapterView.On
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         view.setBackgroundColor(0xffffffff);
         text = parent.getItemAtPosition(position).toString();
-        Intent intent = new Intent(getApplicationContext(),CanvasActivity.class);
+        Intent intent = new Intent(getApplicationContext(), CanvasActivity.class);
         String CurrentLang = Locale.getDefault().getLanguage();
         if (CurrentLang.equals(Locale.ENGLISH.toString())) {
             int color = Color.parseColor(text);
             intent.putExtra(key, color);
-        }
-        intent.putExtra("key2",text);
-        startActivity(intent);
+        } else {
+            if (text.equals("Blanco")) {
+                int color = Color.WHITE;
+            }
+            if (text.equals("Rojo")) {
+                int color = Color.RED;
+            }
+            if (text.equals("Azul")) {
+                int color = Color.BLUE;
+            }
+            if (text.equals("Verde")) {
+                int color = Color.GREEN;
+            }
+            if (text.equals("Amarillo")) {
+                int color = Color.YELLOW;
+            }
+            if (text.equals("Verde azulado")) {
+                int color = Color.parseColor("Teal");
+                       // 0x008080;
+            }
+            if (text.equals("Cian")) {
+                int color = Color.CYAN;
+            }
+            if (text.equals("Lima")) {
+                int color = 0x00FF00;
+            }
+            if (text.equals("Armada")) {
+                int color = Color.RED;
+            }
+            if (text.equals("Púrpura")) {
+                int color = 0x800080;
+            }
+            if (text.equals("Plata")) {
+                int color = 0xC0C0C0;
+            }
+            intent.putExtra("key2", text);
+            startActivity(intent);
 
+        }
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
